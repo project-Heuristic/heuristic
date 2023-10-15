6cb455ecc9def6d6f742eb3858143d21db06e21a
Brahma behera <75154554+ironman2211@users.noreply.github.com>
2023-10-15 16:27:30 +0530

    Update videocall.js-->

--- src/students/components/videocall.js
+++ src/students/components/videocall.js
@@ -10,7 +10,7 @@ import io from "socket.io-client"
 import "./videoCall.scss"
 
 
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect('https://heuristic-server.onrender.com/')
 function VideoCall() {
 	const [ me, setMe ] = useState("")
 	const [ stream, setStream ] = useState()
